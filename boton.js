5f06971732e7f767b6f6b9df6543c4cb2334a60b
Unknown <tm.rodrigo@gmail.com>
2018-01-30 16:21:20 -0300

    ups

--- boton.js
+++ boton.js
@@ -1,13 +1,7 @@
 document.body.onload = function() {
-  chrome.storage.sync.get("data", function(items) {
-    if (!chrome.runtime.error) {
-      if (items.data) {
-				var url = "'http://" +items.data+"'"
-				console.log(window.location.href + " -- " + url)
-				if (window.location.href !== url) {
-					document.body.innerHTML += '<button id="cerrar" type="button" name="button" onclick="window.location='+url+'" style="position:fixed;bottom:30px;left:30px; background-color:red;color:white;padding:16px;z-index:10000;border:0px;cursor:pointer;font-size:64px;border-radius:8px;box-shadow:3px 3px 6px black">Cerrar</button>';
-				}
-      }
-    }
-  });
+  var url = "'http://www.anses.gob.ar/autogestion'"
+  
+  if (window.location.href !== url) {
+    document.body.innerHTML += '<button id="cerrar" type="button" name="button" onclick="window.location='+url+'" style="position:fixed;bottom:30px;left:30px; background-color:red;color:white;padding:16px;z-index:10000;border:0px;cursor:pointer;font-size:64px;border-radius:8px;box-shadow:3px 3px 6px black">Cerrar</button>';
+  }
 }
